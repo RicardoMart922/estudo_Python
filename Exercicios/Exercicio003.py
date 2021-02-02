8fb15c05b6a420859c8da0c9d895f8d5e8a556ae
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-02-02 08:49:47 -0300

    Update Exercicio003.py

--- Exercicios/Exercicio003.py
+++ Exercicios/Exercicio003.py
@@ -1,4 +1,4 @@
 numero1 = int(input('Digite um número: '))
 numero2 = int(input('Digite um número: '))
 soma = (numero1+numero2)
-print('A soma de {} com {} é {}', numero1, numero2, soma)
+print('A soma de {} com {} é {}' .format(numero1, numero2, soma))
