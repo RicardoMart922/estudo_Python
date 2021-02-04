7c9956f019b8c3b42bd7e1583c46c67fabf0ec89
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-02-04 01:42:55 -0300

    Exercicios de Python

--- Exercicios/Exercicio020.py
+++ Exercicios/Exercicio020.py
@@ -1,4 +1,6 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
-import math
+from math import radians, sin, cos, tan
 angulo = float(input('Informe o valor do ângulo: '))
-print('Seno = {}\nCosseno = {}\nTangente = {}'.format(math.sin(angulo), math.cos(angulo), math.tan(angulo)))
+print('Seno = {:.2f}'.format(sin(radians(angulo))))
+print('Cosseno = {:.2f}'.format(cos(radians(angulo))))
+print('Tangente = {:.2f}'.format(tan(radians(angulo))))
---
+++ Exercicios/Exercicio021.py
@@ -0,0 +1,8 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+from random import choice
+aluno1 = str(input('Primeiro aluno: '))
+aluno2 = str(input('Segundo aluno: '))
+aluno3 = str(input('Terceiro aluno: '))
+aluno4 = str(input('Quarto aluno: '))
+alunos = [aluno1, aluno2, aluno3, aluno4]
+print('O aluno(a) que vai apagar o quadro é o(a) {}'.format(choice(alunos))) 
