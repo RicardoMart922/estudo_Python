f18f27a990e93e813ee46a23edc96bc1670ce5ff
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-02-04 23:02:49 -0300

    Exercicio de Python

--- Exercicios/Exercicio025.py
+++ Exercicios/Exercicio025.py
@@ -1,3 +1,5 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 nome = str(input('Digite o nome da cidade: '))
-
+lista = nome.split()
+comeco = 'Santo' in lista[0]
+print(comeco)
--- Exercicios/Exercicio027.py
+++ Exercicios/Exercicio027.py
@@ -2,3 +2,10 @@
 # Quantas vezes aparece a letra "A";
 # Em que posição ela aparece a primeira vez;
 # Em que posição ela aparece a última vez.
+frase = str(input('Digite uma frase qualquer: '))
+minuscula = frase.count('a')
+maiuscula = frase.count('A')
+quantidadeA = (maiuscula + minuscula)
+frase.find('a')
+frase.find('A')
+print(quantidadeA)
