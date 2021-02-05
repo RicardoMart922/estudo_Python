d3522a6e6574bece031bb37507d9622083715fb1
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-02-04 22:21:30 -0300

    Exercicios de Python

--- Exercicios/Exercicio025.py
+++ Exercicios/Exercicio025.py
@@ -1,1 +1,3 @@
-# Crue um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
+nome = str(input('Digite o nome da cidade: '))
+
