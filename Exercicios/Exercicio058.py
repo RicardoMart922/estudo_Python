859b91ac4758125acbf0449e778616c2754fe919
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-02-19 00:28:54 -0300

    Exercicio de Python

--- Exercicios/Exercicio058.py
+++ Exercicios/Exercicio058.py
@@ -6,8 +6,6 @@ media = 0
 nomemasc = ''
 velho = 0
 qtdmulheres = 0
-interadorM = 0
-interadorF = 0
 for i in range(1, 5):
     print('----- {}º PESSOA -----'.format(i))
     nome = str(input('Nome: '))
@@ -17,19 +15,11 @@ for i in range(1, 5):
     if i == 1 and sexo in 'MASCULINOmasculinoMasculinoMm':
         velho = idade
         nomemasc = nome
-        interadorM += 1
     if sexo in 'MASCULINOmasculinoMasculinoMm' and idade > velho:
         velho = idade
         nomemasc = nome
     if sexo in 'FEMININOfemininoFemininoFf' and idade < 20:
-        interadorF += 1
         qtdmulheres += 1
 print('A média de idade do grupo é {:.1f}'.format(media / 4.0))
-if interadorM != 0:
-    print('O nome do homem mais velho é {}'.format(nomemasc))
-else:
-    print('Não há nenhum homem nesse grupo.')
-if sexo in 'FEMININOfemininoFemininoFf' and interadorF != 0: 
-    print('A quantidade de mulheres com menos de 20 anos é {}'.format(qtdmulheres))
-else:
-    print('Não há nenhuma mulher nesse grupo.')
+print('O nome do homem mais velho {}'.format(nomemasc))
+print('A quantidade de mulheres com menos de 20 anos é {}'.format(qtdmulheres))
