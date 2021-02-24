6afbd0bb63589d6331bf1d7e452561c1347a0f6f
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-02-23 22:45:27 -0300

    Exercicios de Python

--- Exercicios/Exercicio064.py
+++ Exercicios/Exercicio064.py
@@ -2,23 +2,21 @@
 a1 = float(input('Informe o primeiro termo da P.A: '))
 razao = int(input('Informe o razão da P.A: '))
 elemento = a1
-termo = -1
+qtdtermos = 0
 i = 1
-j = 10
+j = 0
 print('Os 10 Primeiros Termos')
 while i < 11:
     print('{}º Termo = {}'.format(i, elemento))
     elemento += razao
     i += 1
-print('Você quer mostrar mais alguns termos? [S/N] ')
-opcao = str(input('Opção: ')).upper()
-if opcao == 'S':
-    qtdtermos = int(input('Quantidade de termos: '))
-    while qtdtermos != 0:
-        while j < qtdtermos:
-            print('{}º Termo = {}'.format(i, elemento))
-            elemento += razao
-            j += 1
-        qtdtermos = int(input('Quantidade de termos: '))
-else:
-    print('Fim!')
+qtdtermos = int(input('Você quer mostrar mais quantos termos? '))
+while qtdtermos != 0:
+    while j < qtdtermos:
+        print('{}º Termo = {}'.format(i, elemento))
+        elemento += razao
+        i += 1 
+        j += 1
+    qtdtermos = int(input('Você quer mostrar mais quantos termos? '))
+    j = 0
+print('Ok, encerrando o programa!')
