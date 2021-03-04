3c98009486211b68aaeceb1fb872035529d86f28
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-03-03 23:08:21 -0300

    Exercicios de Python

--- Exercicios/Exercicio069.py
+++ Exercicios/Exercicio069.py
@@ -1,5 +1,6 @@
 # Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador PERDER, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
 from random import randint
+computador = 0
 jogada = 0
 escolha = ''
 soma = 0
--- Exercicios/Exercicio070.py
+++ Exercicios/Exercicio070.py
@@ -2,3 +2,47 @@
 # A) Quantas pessoas tem mais de 18 anos.
 # B) Quantos homens foram cadastrados.
 # C) Quantas mulheres tem menos de 20 anos.
+maisdezoito = 0
+qtdmulheres = 0
+qtdhomens = 0
+idade = 0
+opcao = ''
+sexo = ''
+print('-= Informe a idade e o sexo para o cadastro =-')
+while True:
+    idade = int(input('Idade: '))
+    if idade > 18:
+        maisdezoito += 1
+    while True:
+        sexo = str(input('Sexo [M/F]: ')).upper()
+        if sexo == 'M' or sexo == 'F':
+            if sexo == 'M':
+                qtdhomens += 1
+            if sexo == 'F' and idade < 20:
+                qtdmulheres += 1
+            break
+    while True:
+        opcao = str(input('Quer continuar [S/N]: ')).upper()
+        if opcao == 'S' or opcao == 'N':
+            break
+    if opcao == 'N':
+        break
+if maisdezoito == 0:
+    print('Nenhuma pessoa com mais de 18 anos foi cadastrada.')
+elif maisdezoito == 1:
+    print('Foi cadastrado uma pessoa com mais de 18 anos.')
+else:
+    print(f'Foi cadastrado {maisdezoito} pessoas com mais de 18 anos.')
+if qtdhomens == 0:
+    print('Nenhum homem foi cadastrado.')
+elif qtdhomens == 1:
+    print('Apenas um homem foi cadastrado.')
+else:
+    print(f'A quantidade de homens cadastrados foi {qtdhomens}.')
+if qtdmulheres == 0:
+    print('Nenhuma mulher com menos de 20 anos foi cadastrada.')
+elif qtdmulheres == 1:
+    print('Apenas uma mulher com menos de 20 anos foi cadastrada.')
+else:
+    print(f'A quantidade de mulheres com menos de 20 anos que foram cadastradas foi {qtdmulheres}.')
+    
