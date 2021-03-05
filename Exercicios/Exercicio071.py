005121062b656f602171fc25aa50c0c3d28b08cb
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-03-04 22:30:06 -0300

    Exercicios de Python

--- Exercicios/Exercicio071.py
+++ Exercicios/Exercicio071.py
@@ -2,3 +2,19 @@
 # A) Qual é o total gasto na compra.
 # B) Quantos produtos custam mais de R$1000.
 # C) Qual é o nome do produto mais barato.
+opcao = ''
+nome = ''
+preco = 0
+gasto = 0
+i = 0
+while(True):
+    nome = str(input('Nome: '))
+    gasto += preco
+    if preco < 1000:
+        i += 1 
+    preco = float(input('Preço: '))
+    
+    opcao = str(input('Quer continuar [S/N]: ')).upper()
+    if opcao == 'N':
+        break
+    
