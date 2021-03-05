32851593ca0830a62618d84747bd2b20a6a1f72a
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-03-04 22:42:54 -0300

    Exercicios de Python

--- Exercicios/Exercicio071.py
+++ Exercicios/Exercicio071.py
@@ -4,17 +4,25 @@
 # C) Qual é o nome do produto mais barato.
 opcao = ''
 nome = ''
+nomeprodutobarato = ''
 preco = 0
-gasto = 0
+totgasto = 0
+menorpreco = 0
+
 i = 0
 while(True):
     nome = str(input('Nome: '))
-    gasto += preco
-    if preco < 1000:
-        i += 1 
     preco = float(input('Preço: '))
-    
+    if preco < menorpreco:
+        menorpreco = preco
+        nomeprodutobarato = nome
+    totgasto += preco
+    menorpreco = preco
+    if preco > 1000:
+        i += 1 
     opcao = str(input('Quer continuar [S/N]: ')).upper()
     if opcao == 'N':
         break
-    
+print(f'O total gasto na compra foi R${totgasto:.2f}')
+print(f'A quantidade de produtos que  custam mais de R$1000,00 é {i}')
+print('O nome do produto mais barato é {}.'.format(nomeprodutobarato))
