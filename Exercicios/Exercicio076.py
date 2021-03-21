22aa6fb6e3016243a019dd1223e9c74fcfa33619
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-03-20 23:14:46 -0300

    Exercicios de Python

--- Exercicios/Exercicio076.py
+++ Exercicios/Exercicio076.py
@@ -2,3 +2,23 @@
 # A) Quantas vezes apareceu o valor 9.
 # B) Em que posição foi digitado o primeiro valor 3.
 # C) Quais foram os números pares.
+numero1 = int(input('Digite o 1º número: '))
+numero2 = int(input('Digite o 2º número: '))
+numero3 = int(input('Digite o 3º número: '))
+numero4 = int(input('Digite o 4º número: '))
+tupla = (numero1, numero2, numero3, numero4)
+qtd = 0
+pos = 0
+for cont in range(0, len(tupla)):
+    if tupla[cont] == 9:
+        qtd += 1 
+for p in range(0, len(tupla)):
+    if tupla[p] == 3:
+        pos = p
+        break
+print(f'O número 9 apareceu {qtd} vezes.')
+print(f'O 1º número três apareceu na posição {pos}')
+for i in range(0, len(tupla)):
+    if tupla[i] % 2 == 0:
+        print(f'Os números pares da tupla são: {tupla[i]}')
+    
