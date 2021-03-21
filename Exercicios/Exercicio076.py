68f8ec9b57352eb4f00ed656cfbb3a0db3e1f4be
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-03-20 23:19:44 -0300

    Exercicios de Python

--- Exercicios/Exercicio076.py
+++ Exercicios/Exercicio076.py
@@ -7,6 +7,7 @@ numero2 = int(input('Digite o 2º número: '))
 numero3 = int(input('Digite o 3º número: '))
 numero4 = int(input('Digite o 4º número: '))
 tupla = (numero1, numero2, numero3, numero4)
+par = 0
 qtd = 0
 pos = 0
 for cont in range(0, len(tupla)):
@@ -16,9 +17,14 @@ for p in range(0, len(tupla)):
     if tupla[p] == 3:
         pos = p
         break
-print(f'O número 9 apareceu {qtd} vezes.')
-print(f'O 1º número três apareceu na posição {pos}')
+if qtd == 1:
+    print(f'O número 9 apareceu {qtd} vez.')
+else:
+    print(f'O número 9 apareceu {qtd} vezes.')
+print(f'O 1º número três apareceu na posição {pos+1}')
+print(f'Os números pares da tupla são: ')
 for i in range(0, len(tupla)):
     if tupla[i] % 2 == 0:
-        print(f'Os números pares da tupla são: {tupla[i]}')
+        par = tupla[i]
+        print(f'{par}, ', end='')
     
