9363aae6d40d4d2169f352d6bf8764df65ebd1e1
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-03-29 23:54:04 -0300

    Exercicios de Python

--- Exercicios/Exercicio074.py
+++ Exercicios/Exercicio074.py
@@ -3,7 +3,9 @@
 # B) Os últimos 4 colocados da tabela.
 # C) Uma lista com os times em alfabética.
 # D) Em que posição na tabela está o time da Ceará.
-tabela = ('Flamengo - RJ', 'Internacional - RS', 'Atlético - MG', 'São Paulo - SP', 'Fluminense - RJ', 'Grêmio - RS', 'Palmeiras - SP', 'Santos - SP', 'Athletico Paranaense - PR', 'Red Bull Bragantino - SP', 'Ceará - CE', 'Corinthians - SP', 'Atlético - GO', 'Bahia - BA', 'Sport - PE', 'Fortaleza - CE', 'Vasco da Gama - RJ', 'Goiás - GO', 'Coritiba - PR', 'Botafogo - RJ') 
+tabela = ('Flamengo - RJ', 'Internacional - RS', 'Atlético - MG', 'São Paulo - SP', 'Fluminense - RJ', 'Grêmio - RS', 'Palmeiras - SP', 'Santos - SP', 'Athletico Paranaense - PR', 'Red Bull Bragantino - SP', 'Ceará - CE', 'Corinthians - SP', 'Atlético - GO', 'Bahia - BA', 'Sport - PE', 'Fortaleza - CE', 'Vasco da Gama - RJ', 'Goiás - GO', 'Coritiba - PR', 'Botafogo - RJ')
+print(f'A lista de Times: {tabela}') 
+print('-=-' * 10)
 print(f'Os primeiros 5 colocados:')
 for i in range(0, 5):
     print(f'{i+1}º - {tabela[i]}')
