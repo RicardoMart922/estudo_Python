4951dbd725b95cda5e820c7d69530ad21b1c2933
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-03-31 23:17:29 -0300

    Exercicios de Python

--- Exercicios/Exercicio077.py
+++ Exercicios/Exercicio077.py
@@ -1,10 +1,12 @@
 # Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
 # No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 tupla = ('Lápis', 0.65, 'Caneta', 0.75, 'Caderno', 13.50, 'Mochila', 26.00)
-p = 0
-s = 1
-for i in range(0, 8):
-    print(f'{tupla[p]}.........R${tupla[s]}')
-    p = p + 2
-    s = s + 2
-print('Fim.')
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-'*40)
+for pos in range(0, len(tupla)):
+    if pos % 2 == 0:
+        print(f'{tupla[pos]:.<30}', end='')
+    else:
+        print(f'R$ {tupla[pos]:>7.2f}')
+print('-'*40)
