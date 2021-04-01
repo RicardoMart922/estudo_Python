0f739bdbb79f0c42c4da557350fd3b83def17ceb
Ricardo Martins <ricardo.martins.6373@gmail.com>
2021-03-31 23:28:04 -0300

    Exercicios de Python

--- Exercicios/Exercicio078.py
+++ Exercicios/Exercicio078.py
@@ -1,6 +1,7 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 tupla = ('Cachorro', 'Gato', 'Casa', 'Predio', 'Carro', 'Moto', 'Musica')
-for i in range(0, len(tupla)):
-    print(f'{tupla[i]} = ')
-    
-
+for i in tupla:
+    print(f'\nNa palavra {i} temos ', end='')
+    for j in i:
+        if j.lower() in 'aeiou':
+            print(j, end=' ')
